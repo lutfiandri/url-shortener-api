7dc3c6c4ec08d0b0a4f67a7cae2e8e6ce9d0caec
Lutfi Andriyanto <lutfiandri232@gmail.com>
2022-04-20 12:39:06 +0700

    test(repository): findall, findbyid, delete

--- repository/url_repo_test.go
+++ repository/url_repo_test.go
@@ -38,6 +38,41 @@ func TestMain(m *testing.M) {
 	os.Remove("../.tmp/url-shortener.db")
 }
 
+func TestFindAll(t *testing.T) {
+	urlRepository := repository.NewUrlRepository(database)
+
+	urls := []entity.Url{
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://google.com",
+			UserId:  shortuuid.New(),
+		},
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://facebook.com",
+			UserId:  shortuuid.New(),
+		},
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://manjaro.org",
+			UserId:  shortuuid.New(),
+		},
+	}
+
+	for _, url := range urls {
+		database.Create(&url)
+	}
+
+	results, err := urlRepository.FindAll()
+
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, urls, results)
+
+	for _, url := range urls {
+		database.Delete(&url)
+	}
+}
+
 func TestFindById_Positive(t *testing.T) {
 	urlRepository := repository.NewUrlRepository(database)
 
@@ -60,6 +95,46 @@ func TestFindById_Negative(t *testing.T) {
 	assert.Empty(t, result)
 }
 
+func TestFindByUserId(t *testing.T) {
+	urlRepository := repository.NewUrlRepository(database)
+
+	urls := []entity.Url{
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://google.com",
+			UserId:  "EAsgVGodDaX32nQScND35a",
+		},
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://facebook.com",
+			UserId:  "EAsgVGodDaX32nQScND35a",
+		},
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://manjaro.org",
+			UserId:  "JLrQVwVRYGXJhZQX7VbXCT",
+		},
+		{
+			Id:      shortuuid.New(),
+			LongUrl: "https://ubuntu.com",
+			UserId:  "",
+		},
+	}
+
+	for _, url := range urls {
+		database.Create(&url)
+	}
+
+	results, err := urlRepository.FindByUserId("EAsgVGodDaX32nQScND35a")
+
+	assert.NoError(t, err)
+	assert.ElementsMatch(t, urls[0:2], results)
+
+	for _, url := range urls {
+		database.Delete(&url)
+	}
+}
+
 func TestCreate(t *testing.T) {
 	urlRepository := repository.NewUrlRepository(database)
 
@@ -72,4 +147,26 @@ func TestCreate(t *testing.T) {
 	assert.Equal(t, url, result)
 
 	database.Delete(&url)
+}
+
+func TestDeleteById_Positive(t *testing.T) {
+	urlRepository := repository.NewUrlRepository(database)
+
+	database.Create(&url)
+
+	err := urlRepository.DeleteById(url.Id)
+
+	var result entity.Url
+	database.First(&result, "id = ?", url.Id)
+
+	assert.NoError(t, err)
+	assert.Empty(t, result)
+}
+
+func TestDeleteById_Negative(t *testing.T) {
+	urlRepository := repository.NewUrlRepository(database)
+
+	err := urlRepository.DeleteById(url.Id)
+
+	assert.Error(t, err)
 }
